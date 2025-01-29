f0765b36d08a7092c2f419afd6713f2cff13c417
Zeynep Şevval Şener <sevval_snr@outlook.com>
2025-01-30 02:46:28 +0300

    tryying

--- MyApp/models.py
+++ MyApp/models.py
@@ -52,8 +52,8 @@ class Contact(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
+    additional_field = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.user.username
+
