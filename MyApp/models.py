b16c7c408c783e73ead66608ef66042be393f7b7
Zeynep Şevval Şener <sevval_snr@outlook.com>
2025-01-30 02:30:31 +0300

    so much things

--- MyApp/models.py
+++ MyApp/models.py
@@ -5,11 +5,11 @@ class Car(models.Model):
     car_name = models.CharField(max_length=100)
     car_desc = models.TextField()
     image = models.ImageField(upload_to='cars/')
-    transmission = models.CharField(max_length=20, choices=[('Automatic', 'Automatic'), ('Manual', 'Manual')])
-    deposit = models.DecimalField(max_digits=10, decimal_places=2)
-    mileage = models.PositiveIntegerField()
-    age = models.PositiveIntegerField()
-    cost_per_day = models.DecimalField(max_digits=10, decimal_places=2)
+    transmission = models.CharField(max_length=20, choices=[('Automatic', 'Automatic'), ('Manual', 'Manual')], null=True, blank=True)
+    deposit = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    mileage = models.PositiveIntegerField(default=0)
+    age = models.PositiveIntegerField(default=5)
+    cost_per_day = models.DecimalField(max_digits=10, decimal_places=2, default=100)
 
     def __str__(self):
         return self.car_name
